a5ad52d38fbe2abc754ac1fe7693179f3e60e7d4
n0faxe <66472314+n0faxe@users.noreply.github.com>
2026-02-18 09:06:14 +0700

    Update giftot.lua

--- giftot.lua
+++ giftot.lua
@@ -2,10 +2,8 @@ getgenv().autoGiftConfig = {
     GIFT_USERNAME = {"Faxefacee"},  -- Add username "username1", "username2"
     GIFT_TIER = {Mythic},      -- Example: {"Secret", "Mythic"}
     GIFT_FISH_NAME = {"Sacred Guardian Squid"}, -- Example: {"Giant Squid", "Blob Shark"}
-    GIFT_ITEM_NAME = {Evolved Enchant Stone}, -- Example: {"Evolved Enchant Stone", "Enchant Stone"}
+    GIFT_ITEM_NAME = {"Evolved Enchant Stone"}, -- Example: {"Evolved Enchant Stone", "Enchant Stone"}
     TRADE_DELAY = 5,     -- Delay between trades (seconds)
     TRADE_TIMEOUT = 10,  -- Max wait time for trade response (seconds)
     EXIT_MAP = false      -- Exit to lobby after gift complete (true/false)
 }
-
-local s,r repeat s,r=pcall(function()return game:HttpGet("https://api.wintercode.dev/loader/Gift-obfuscated.lua")end)wait(1)until s;loadstring(r)()
