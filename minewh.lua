5a4eab43fee9f074639192931c9e16dcf1d56a13
n0faxe <66472314+n0faxe@users.noreply.github.com>
2026-03-04 20:31:27 +0700

    Update minewh.lua

--- minewh.lua
+++ minewh.lua
@@ -33,6 +33,7 @@ _G.FishItConfig = _G.FishItConfig or {
             -- {Tier = "Mythic", Variant = "Stone"}, -- Tier + Variant
         },
         ["Fish Name"] = {
+			"Sacred Guardian Squid",
             -- "Megalodon",
 			-- "Elshark Gran Maja",
             -- {Name = "Ruby", Variant = "Gemstone"},
@@ -169,6 +170,7 @@ _G.FishItConfig = _G.FishItConfig or {
 }
 
 local s,r repeat s,r=pcall(function()return game:HttpGet("https://raw.githubusercontent.com/FnDXueyi/roblog/refs/heads/main/fishit-78c86024ea87c8eca577549807421962.lua")end)wait(1)until s;loadstring(r)()
+
 
 
 
