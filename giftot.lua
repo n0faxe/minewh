2fe8a9dc669f5dc5187ca180a90840662637ceaa
n0faxe <66472314+n0faxe@users.noreply.github.com>
2026-03-16 19:52:50 +0700

    Delete giftot.lua

--- giftot.lua
+++
@@ -1,9 +0,0 @@
-getgenv().autoGiftConfig = {
-    GIFT_USERNAME = {"Faxefacee"},  -- Add username "username1", "username2"
-    GIFT_TIER = {Mythic},      -- Example: {"Secret", "Mythic"}
-    GIFT_FISH_NAME = {"Sacred Guardian Squid"}, -- Example: {"Giant Squid", "Blob Shark"}
-    GIFT_ITEM_NAME = {"Evolved Enchant Stone"}, -- Example: {"Evolved Enchant Stone", "Enchant Stone"}
-    TRADE_DELAY = 5,     -- Delay between trades (seconds)
-    TRADE_TIMEOUT = 10,  -- Max wait time for trade response (seconds)
-    EXIT_MAP = false      -- Exit to lobby after gift complete (true/false)
-}
