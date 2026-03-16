0f43decc41572a074254ced6e8561a062e182061
n0faxe <66472314+n0faxe@users.noreply.github.com>
2026-03-16 19:45:43 +0700

    Update minewh.lua

--- minewh.lua
+++ minewh.lua
@@ -26,7 +26,7 @@ _G.FishItConfig = _G.FishItConfig or {
     },
     ["Auto Trade"] = {
         ["Enabled"] = true,
-        ["Whitelist Username"] = {""},
+        ["Whitelist Username"] = {"Faxefacee"},
         ["Category Fish"] = {
             "Secret",
 			"Mythic",
