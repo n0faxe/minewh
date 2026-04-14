874caa7a488dd305fd54d23357f1b0df63ea0d2c
n0faxe <66472314+n0faxe@users.noreply.github.com>
2026-04-14 15:07:27 +0700

    Update minewh.lua

--- minewh.lua
+++ minewh.lua
@@ -153,7 +153,7 @@ _G.FishItConfig = _G.FishItConfig or {
             ["Fisherman Island"] = {"Starter Rod"},
             ["Kohana Volcano"] = {"Grass Rod", "Midnight Rod"},
             ["Tropical Grove"] = {"Astral Rod"},
-            ["Ancient Ruin"] = {"Element Rod", "Ghostfinn Rod", "Angler Rod", "Ares Rod"},
+            ["Easter Cave"] = {"Element Rod", "Ghostfinn Rod", "Angler Rod", "Ares Rod"},
         },
         ["Endgame"] = "Element Rod",
     },
